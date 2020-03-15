119f8e4b15f3537359258f61ffdfcc365b09b93d
Justin <futzy261@gmail.com>
2020-03-16 00:23:51 +0100

    :bug: Fixed Leaderboard

--- src/commands/economy/leaderboard.js
+++ src/commands/economy/leaderboard.js
@@ -7,10 +7,19 @@ module.exports = {
     cooldown: 5,
     async execute(message, args, client, Embeds) {
         client.con.query("SELECT userid, cookies FROM users ORDER BY cookies DESC LIMIT 10;", [], async (e, r) => {
+            let list = "";
+            for (let result of r) {
+                let user = await client.users.cache.find(i => i.id === result.userid);
+                if (user) {
+                    list += `${user.tag}: \`\`${result.cookies} 🍪\`\`\n`
+                } else {
+                    list += `Unknown#0000: \`\`${result.cookies} 🍪\`\`\n`
+                }
+            }
             if (e) {
                 return Embeds.error(message.channel, "\`\`\`js\n" + e + "\`\`\`");
             }
-            return Embeds.uni(message.channel, `${r.map(u => `${client.users.cache.find(i => i.id === u.userid).tag}: ` + `\`\`${u.cookies} 🍪\`\``).join(`\n`)}`, (await client.string(message.guild.id, "command.leaderboard.title")), "", "", "", 0x7289DA);
+            return Embeds.uni(message.channel, list, (await client.string(message.guild.id, "command.leaderboard.title")), "", "", "", 0x7289DA);
         });
     }
 }
