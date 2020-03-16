1315e0651397e1aef59bd2eb8ec33f4cfdb76507
Ron31 <ron3108@gmx.net>
2020-03-16 13:24:13 +0100

    Update String.js

--- src/struct/String.js
+++ src/struct/String.js
@@ -2,6 +2,10 @@ const con = require("./Database.js");
 
 function strings(guild, strings) {
     return new Promise((resolve, reject) => {
+        if(guild == "dm") {
+            let languageSelected = require("./../languages/en_us.json");
+            resolve(languageSelected[strings]);
+        }
         con.query("SELECT * FROM languages WHERE guildid = ?", [guild], async (e, r) => {        
             let language;
 
